e8fbb6eb86a9b980920982e2cb7d286224a6b6e1
Dennis Tye <46984479+dennistye@users.noreply.github.com>
2022-11-16 20:24:48 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="dennis" />
         <p className="description">
           amanda <code>pages/index.js</code>
         </p>
