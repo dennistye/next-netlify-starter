8a060d0ec970ff11372a2dfa4b806550a57342b2
dennistye <dennis.tye@csu.fullerton.edu>
2022-11-22 18:42:41 -0800

    hey there bud

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
 import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
 
 export default function Home() {
   return (
     <div className="container">
-      <Header />
       <Head>
         <meta charset="UTF-8" />
         <meta name="viewport"
@@ -17,8 +14,6 @@ export default function Home() {
           Hello
         </h1>
       </main>
-
-      <Footer />
     </div>
   )
 }
