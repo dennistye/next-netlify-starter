ebb2430d372c9541c2476c928a890e40608e05e3
dennistye <dennis.tye@csu.fullerton.edu>
2022-11-22 18:36:20 -0800

    hey there bud

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div className="container">
+      <Header />
       <Head>
         <meta charset="UTF-8" />
         <meta name="viewport"
